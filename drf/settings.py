1ea4ee48148b54202ae99a03a23f367d6bccca27
GabrielBarrientos99 <gabriel.barrientos.c@uni.pe>
2024-06-18 00:05:36 -0500

    creacion_modelos

--- drf/settings.py
+++ drf/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    # CORS headers si necesitas permitir solicitudes desde otros dominios
+    'corsheaders',
     'coreapi',
     'api',
 ]
@@ -50,6 +52,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # CORS headers middleware
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "drf.urls"
